017f2dcc387483ba6f0b6498044e3adce7ecb392
Bautista <broccella@uade.edu.ar>
2025-03-14 15:34:41 -0300

    Fix: Update base URL and homepage in configuration

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: 'https://bauroccella.github.io/pokedex/',
   plugins: [react()],
   build: {
     assetsInlineLimit: 0,
     emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
   },
 })
