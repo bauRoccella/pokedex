c08affaeb7ba33f23d97a0572f84e18bcba2f7db
Bautista <broccella@uade.edu.ar>
2025-03-14 15:54:51 -0300

    Fix: Simplify entry point strategy with relative base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://bauroccella.github.io/pokedex/',
+  base: './',
   plugins: [react()],
   build: {
     assetsInlineLimit: 0,
